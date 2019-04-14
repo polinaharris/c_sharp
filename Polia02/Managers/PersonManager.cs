4126ab13479999be56fb516a94432d1477cdc3e4
polinaharris <45798905+polinaharris@users.noreply.github.com>
2019-04-14 16:34:57 +0300

    lab02new

---
+++ Polia02/Managers/PersonManager.cs
@@ -0,0 +1,12 @@
+﻿namespace Polia02.Managers
+{
+    public class PersonManager
+    {
+        public static InfoViewModel InfoViewModel { get; set; }
+
+        public static void Update(Person person)
+        {
+            InfoViewModel.Person = person;
+        }
+    }
+}
